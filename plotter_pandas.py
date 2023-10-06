7413ea83cc25f70788ead2ce094f18dc80d917c8
Otso Luukkanen <otso.luukkanen@aalto.fi>
2023-10-06 18:39:47 +0300

    Added more measurement data and refined code to use outputfolders and display time in minutes

--- plotter_pandas.py
+++ plotter_pandas.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 # import typing
@@ -5,18 +6,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas.errors
-
+import pytz
 
 # class OperatorDataSet:
 #     def __init__(self, operator: str, folder_name):
 #         self.operator = operator
 #         df = pd.DataFrame()
 
+csv_output_folder = './csv_out/'
+graph_output_folder = './graph_out/'
 
-folders = ['./Otso', './Atif', './Juho']
-operators = ['DNA', 'Elisa', 'Telia']
-terminals = {'Xiaomi_M2007J3SY':'Xiaomi Mi 10T 5G', 'Xiaomi_23021RAAEG':'Xiaomi Redmi Note 12', 'Samsung_SM-S911B':'Samsung Galaxy S23'}
-
+folders = ['./Otso-Kivenlahti', './Atif-Kivenlahti', './Juho-Kivenlahti', './Otso-Vuosaari', './Atif-Vuosaari',
+           './Juho-Vuosaari']
+operators = ['DNA', 'Elisa', 'Telia', 'DNA', 'Elisa', 'Telia']
+terminals = {'Xiaomi_M2007J3SY': 'Xiaomi Mi 10T 5G', 'Xiaomi_23021RAAEG': 'Xiaomi Redmi Note 12',
+             'Samsung_SM-S911B': 'Samsung Galaxy S23'}
 
 for index, folder in enumerate(folders):
     tables = []
@@ -41,7 +45,7 @@ for index, folder in enumerate(folders):
     df = df.drop_duplicates(subset='time')
     print('Size is:' + str(df.shape))
     print(df.columns)
-    df = df.sort_values(by=['time'])
+    df = df.sort_values(by=['time'], axis=0)
 
     # Finding the device
     terminal = df.iloc[1]['device']
@@ -49,18 +53,29 @@ for index, folder in enumerate(folders):
 
     # Create a boolean column to show where cell ID changes
     df['cell_change'] = df['cellid'] != df['cellid'].shift()
-    df.to_csv(operators[index] + '.csv', sep=';')
+    # Create a column for displaying time in formatted way
+    # tz = pytz.utc
+    df['formatted_time'] = df['time'].apply(
+        lambda x: datetime.datetime.fromtimestamp(round(x / 1000)))
+    # df['formatted_time'] = df['time'].apply(lambda x: pd.Da(x, tz='Europe/Helsinki'))
+    # print(df.columns)
+    df['min_from_start'] = df['formatted_time'].apply(
+        lambda x: (x - df['formatted_time'].iloc[0]).total_seconds() / 60)
+
+    # Save the DataFrame to CSV file
+    df.to_csv(csv_output_folder + operators[index] + str(index) + '.csv', sep=';')
+
     # Plot and save the graph
     colors_indict = {'DNA': '#ff1493', 'Elisa': '#0000ff', 'Telia': '#800080'}
-    df.plot(kind='line', x='time', y='signal', figsize=(20, 5), color=[colors_indict.get(operators[index])])
+    df.plot(kind='line', x='min_from_start', y='signal', figsize=(20, 5), color=[colors_indict.get(operators[index])])
     # df.plot(kind='line', x='time', y='signal')
     plt.title('Operator: ' + operators[index] + ', Device: ' + device)
-    plt.xlabel('Unix timestamp time')
+    plt.xlabel('Minutes from start')
     plt.ylabel('Signal value in dBm')
     for idx, row in df.iterrows():
         if row['cell_change']:
-            plt.axvline(x=row['time'], color='r', linestyle='--', linewidth='0.3')
+            plt.axvline(x=row['min_from_start'], color='r', linestyle='--', linewidth='0.3')
     # df.plot(y='signal')
-    plt.legend([operators[index]], loc='upper right')
-    plt.savefig(operators[index])
+    plt.legend([operators[index]],  loc='upper right')
+    plt.savefig(graph_output_folder + operators[index] + str(index))
     plt.show()
